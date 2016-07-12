6239653268019152aefaeb6ad6d35ebda6041329
Hector Garcia <soseinhg3@gmail.com>
2016-07-12 14:39:55 +0000

    Refactor search router
    - Split search router into route, module, and helper files
    - Set all documents to expire daily at midnight Pacific Time

--- routes/search.js
+++ routes/search.js
@@ -1,53 +1,67 @@
 "use strict";
 
-const express = require("express");
-const router = express.Router();
-const google = require("googleapis");
-const customsearch = google.customsearch("v1");
+const router = require("express").Router();
+const customsearch = require("googleapis").customsearch("v1");
+const moment = require("moment-timezone");
 
-const DEFAULT_SEARCH_PARAMS = Object.freeze({
+let searchResults;
+
+const getImageData = require("../helpers/get-image-data");
+
+const DEFAULT_SEARCH_PARAMS = {
     cx: process.env.CSE_ID,
     auth: process.env.GOOGLE_API_KEY,
     safe: "medium",
     searchType: "image"
-});
-
-function getImageData(searchResult) {
-    return {
-        snippet: searchResult.snippet,
-        url: searchResult.link,
-        size: searchResult.image.width + " × " + searchResult.image.height,
-        context: searchResult.image.contextLink
-    };
-}
+};
 
 router.get("/", function (req, res) {
-    const searchPhrase = req.query.q;
-    if (!searchPhrase) {
-        return res.redirect(301, "/");
-    }
+    if (!req.query.q || !req.query.q.trim()) return res.redirect(301, "/");
+    if (req.query.offset > 9) return res.sendStatus(400);
 
+    const searchPhrase = req.query.q.trim();
     const offset = req.query.offset || 0;
-    if (offset > 9) {
-        return res.sendStatus(400);
+    if (!searchResults) {
+        searchResults = require("../models/searchresults")(req.db);
     }
 
-    const searchParams = Object.assign({}, DEFAULT_SEARCH_PARAMS, {
-        q: searchPhrase,
-        start: offset * 10 + 1
-    });
-    customsearch.cse.list(searchParams, function (err, data) {
-        if (err) {
-            console.error(err.stack);
-            return res.sendStatus(500);
-        }
-        if (!data.items) {
-            return res.end([]);
-        }
+    searchResults
+        .find({
+            searchPhrase,
+            index: {
+                $gte: offset * 10 + 1,
+                $lt: offset * 10 + 11
+            }
+        })
+        .then(docs => {
+            if (docs.length) return res.json(docs.map(getImageData));
 
-        const searchResults = data.items.map(getImageData);
-        res.json(searchResults);
-    });
+            const searchParams = Object.assign({}, DEFAULT_SEARCH_PARAMS, {
+                q: searchPhrase,
+                start: offset * 10 + 1
+            });
+            customsearch.cse.list(searchParams, (err, data) => {
+                if (err) {
+                    console.error(err.stack);
+                    return res.sendStatus(500);
+                }
+                if (!data.items) return res.end([]);
+
+                let docs = data.items.map(getImageData);
+
+                res.json(docs);
+
+                const PTZ = moment.tz("America/Los_Angeles");
+                const expireAt = PTZ.startOf("day").hour(24).toDate();
+                for (let i = 0; i < docs.length; i++) {
+                    docs[i].expireAt = expireAt;
+                    docs[i].index = data.queries.request[0].startIndex + i;
+                    docs[i].searchPhrase = searchPhrase;
+                }
+
+                searchResults.insertMany(docs);
+            });
+        });
 });
 
 module.exports = router;
